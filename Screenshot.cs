9937b56239431f17e77fddad9c4168e224c4dddb
Zach Sherman <zachsherman@cs.uchicago.edu>
2018-09-29 16:33:51 -0500

    initial commit

---
+++ Screenshot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour {
+    public string fileName;
+    int screenshotNum;
+    
+    public void Capture () {
+        string fileNameTemp = fileName + screenshotNum.ToString();
+        ScreenCapture.CaptureScreenshot(fileNameTemp+".png");
+        screenshotNum += 1;
+	}
+	
+}
---
+++ ScreenshotEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Screenshot))]
+public class ScreenshotEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Screenshot screenshot = (Screenshot)target;
+        if (GUILayout.Button("Screenshot"))
+        {
+            screenshot.Capture();
+        }
+    }
+}
